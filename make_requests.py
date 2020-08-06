5985944e5f6256e46dab3d825c5b7015846d8d07
AsornXD <45113995+AsornXD@users.noreply.github.com>
2020-08-06 18:27:15 -0400

    remove client initialization from each function, and made it initialize at the start

--- make_requests.py
+++ make_requests.py
@@ -1,9 +1,11 @@
 import os
 from twitch import TwitchClient
 
+client = TwitchClient(os.environ['CLIENT_ID'])
+
+
 def get_follows(limit, user):
     limit = int(limit)
-    client = TwitchClient(os.environ['CLIENT_ID'])
     users = client.users.translate_usernames_to_ids([user])
     follow_list = []
     for user in users:
@@ -11,7 +13,6 @@ def get_follows(limit, user):
             follow_list.append({'name': i.channel.name, 'date': i.created_at})
     return follow_list
 def check_if_subscribed(user, channel):
-    client = TwitchClient(os.environ['CLIENT_ID'])
     idarray = client.users.translate_usernames_to_ids([user])
     print (idarray)
     check = client.check_subscribed_to_channel(idarray[0], 44322889)
@@ -23,9 +24,7 @@ def check_if_subscribed(user, channel):
         return user, "is not subscirbed to", client.channels.get_by_id(44322889).name
 
 def get_topgames(templimit):
-    client = TwitchClient(os.environ['CLIENT_ID'])
     return client.games.get_top(int(templimit))
 
 def get_channels(search_query, search_limit):
-    client = TwitchClient(os.environ['CLIENT_ID'])
     return client.search.channels(search_query, limit = int(search_limit))
